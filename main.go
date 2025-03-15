4cea38af3d3a71f2b2066f53188afee54dc7c88b
Patrick Radu Stanca Munteanu <120978917+PatrickRadu@users.noreply.github.com>
2025-03-15 11:26:54 +0200

    the first commit

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+type Weather struct {
+	Address     string        `json:"address"`
+	RequestDate string        `json:"requestdate"`
+	Days        []WeatherDays `json:"days"`
+}
+
+type WeatherDays struct {
+	Description    string  `json:"description"`
+	Temperature    float64 `json:"temperature"`
+	TemperatureMax float64 `json:"temperaturemax"`
+	TemperatureMin float64 `json:"temperaturemin"`
+	Datetime       string  `json:"datetime"`
+	FeelsLike      float64 `json:"feelslike"`
+}
+
+var apiKey string
+
+func main() {
+	e := echo.New()
+	err := godotenv.Load()
+	if err != nil {
+		e.Logger.Fatal("Error loading .env file")
+	}
+	secretKey := os.Getenv("key")
+	apiKey = secretKey
+	e.GET("/", getWeather)
+	e.POST("/location", getWeatherInLocation)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getWeatherInLocation(c echo.Context) error {
+	location := c.FormValue("location")
+	resp, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?unitGroup=us&include=days&key=" + apiKey + "&contentType=json")
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get weather data from api")
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to read weather data body")
+	}
+	jsonData := string(body)
+	var weatherData map[string]interface{}
+	err = json.Unmarshal([]byte(jsonData), &weatherData)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to parse weather data")
+	}
+	varResponse := Weather{}
+	varResponse.Address = weatherData["address"].(string)
+	varResponse.RequestDate = time.Now().Format("2006-01-02")
+	for _, day := range weatherData["days"].([]interface{}) {
+		dayMap := day.(map[string]interface{})
+		varResponse.Days = append(varResponse.Days, WeatherDays{
+			Description:    dayMap["description"].(string),
+			Temperature:    dayMap["temp"].(float64),
+			TemperatureMax: dayMap["tempmax"].(float64),
+			TemperatureMin: dayMap["tempmin"].(float64),
+			Datetime:       dayMap["datetime"].(string),
+			FeelsLike:      dayMap["feelslike"].(float64),
+		})
+	}
+	return c.JSON(http.StatusOK, varResponse)
+}
+
+func getWeather(c echo.Context) error {
+	resp, err := http.Get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Cluj?unitGroup=us&include=days&key=" + apiKey + "&contentType=json")
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get weather data")
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to read weather data body")
+	}
+	// i want to make this to json
+	jsonData := string(body)
+	var weatherData map[string]interface{}
+	err = json.Unmarshal([]byte(jsonData), &weatherData)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to parse weather data")
+	}
+
+	varResponse := Weather{}
+	varResponse.Address = weatherData["address"].(string)
+	varResponse.RequestDate = time.Now().Format("2006-01-02")
+	varResponse.Days = []WeatherDays{}
+	for _, day := range weatherData["days"].([]interface{}) {
+		dayMap := day.(map[string]interface{})
+		varResponse.Days = append(varResponse.Days, WeatherDays{
+			Description:    dayMap["description"].(string),
+			Temperature:    dayMap["temp"].(float64),
+			TemperatureMax: dayMap["tempmax"].(float64),
+			TemperatureMin: dayMap["tempmin"].(float64),
+			Datetime:       dayMap["datetime"].(string),
+			FeelsLike:      dayMap["feelslike"].(float64),
+		})
+	}
+	return c.JSON(http.StatusOK, varResponse)
+}
